b1b63459e994b6d16b9c0e2b803cc757449e24d3
himeno61 <binkiewicz.luke@gmail.com>
2023-05-24 19:34:04 +0200

    Update handler

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,8 +1,33 @@
 import {ApiHandler} from "sst/node/api";
 import {performQuery} from "./graphql";
+import {Pool} from "pg";
 import {createPostGraphileSchema} from "postgraphile";
+import {GraphQLSchema, printSchema} from "graphql";
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('error', error => {
+    console.error('Postgres generated pool error!', error)
+})
+
+const options = {
+    connection: process.env.DATABASE_URL,
+    schema: [process.env.SCHEMA || "public"],
+    jwtSecret: process.env.DEFAULT_ROLE  || "SUPER_SECRET_JWT_SECRET",
+    defaultRole:  process.env.DEFAULT_ROLE  || "guest",
+    jwtTokenIdentifier: process.env.JWT_IDENTIFIER || "app.jwt_token",
+    watch: false,
+}
+const postgraphileSchemaPromise =  createPostGraphileSchema(pool, options.schema);
 
 export const handler = ApiHandler(async (_evt) => {
+    if(!_evt.body){
+        return {
+            statusCode: 400,
+        }
+    }
     const reqBody = JSON.parse(_evt.body!)
 
     const authorizationHeader = _evt.headers["authorization"] || _evt.headers["Authorization"];
@@ -12,12 +37,24 @@ export const handler = ApiHandler(async (_evt) => {
         }
     }
 
-    const jwtToken = authorizationHeader.substring(7, authorizationHeader.length);
+    let postgraphileSchema: GraphQLSchema;
 
+    console.log('Awaiting schema')
+    try {
+        postgraphileSchema = await postgraphileSchemaPromise
+    } catch (error) {
+        console.error('Rebuilding schema after connection loss')
+        postgraphileSchema = await createPostGraphileSchema(pool, options.schema, options);
+    }
+
+    const jwtToken = authorizationHeader.substring(7, authorizationHeader.length);
     console.log('Query:', reqBody.query)
     console.log('Variables:', reqBody.variables)
 
-    const result = await performQuery(null, reqBody.query, reqBody.variables, jwtToken, reqBody.operationName);
+
+    console.log(printSchema(postgraphileSchema));
+
+    const result = await performQuery(postgraphileSchema, reqBody.query, reqBody.variables, jwtToken);
     console.log('Result:', result)
 
     return {
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -3,16 +3,15 @@ import { StackContext, Api } from "sst/constructs";
 export function API({ stack }: StackContext) {
   const api = new Api(stack, "api", {
     routes: {
-      "GET /": "packages/functions/src/lambda.handler",
-      "POST /schema": {
+      "GET /": {
           function: {
-              handler: "packages/functions/src/generateSchema.handler",
+              handler: "packages/functions/src/lambda.handler",
               environment: {
                   DATABASE_URL: process.env.DATABASE_URL,
                   DEFAULT_ROLE: process.env.DEFAULT_ROLE,
                   JWT_IDENTIFIER: process.env.JWT_IDENTIFIER,
                   JWT_SECRET: process.env.JWT_SECRET
-              },
+              }
           }
       },
       "GET /{proxy+}": {
