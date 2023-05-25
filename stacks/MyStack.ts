183ffe20b6a1602623baa7f8ffdd465147fd8c62
Ty Rauber <tyrauber@mac.com>
2023-05-25 06:51:13 -0400

    chore: rebase and reorganize

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -2,39 +2,37 @@ import { StackContext, Api } from "sst/constructs";
 
 export function API({ stack }: StackContext) {
   const api = new Api(stack, "api", {
+    defaults: {
+      function: {
+        environment: {
+          DATABASE_URL: process.env.DATABASE_URL,
+          DEFAULT_ROLE: process.env.DEFAULT_ROLE,
+          JWT_IDENTIFIER: process.env.JWT_IDENTIFIER,
+          JWT_SECRET: process.env.JWT_SECRET
+        },
+        copyFiles: [
+          {"from": "./packages/functions/src/postgraphile.cache", "to": "packages/functions/src/postgraphile.cache"},
+          {"from": "./.postgraphilerc.js", "to": "packages/functions/src/.postgraphilerc.js"},
+        ],
+        nodejs:{
+          loader: {
+            ".graphql": "text",
+            ".cache": "text"
+          }
+        },
+      }
+    },
     routes: {
-      "GET /": {
+      "ANY /": {
           function: {
-              handler: "packages/functions/src/lambda.handler",
-              environment: {
-                  DATABASE_URL: process.env.DATABASE_URL,
-                  DEFAULT_ROLE: process.env.DEFAULT_ROLE,
-                  JWT_IDENTIFIER: process.env.JWT_IDENTIFIER,
-                  JWT_SECRET: process.env.JWT_SECRET
-              }
+            handler: "packages/functions/src/postgraphile_lambda.handler",
           }
       },
       "ANY /{proxy+}": {
         function: {
-            handler: "packages/functions/src/postgraphile.handler",
-            environment: {
-              DATABASE_URL: process.env.DATABASE_URL,
-              DEFAULT_ROLE: process.env.DEFAULT_ROLE,
-              JWT_IDENTIFIER: process.env.JWT_IDENTIFIER,
-              JWT_SECRET: process.env.JWT_SECRET
-            },
-            nodejs:{
-              loader: {
-                ".graphql": "text",
-                ".cache": "text"
-              }
-            },
-            copyFiles: [
-              {"from": "./packages/functions/src/postgraphile.cache", "to": "packages/functions/src/postgraphile.cache"},
-              {"from": "./.postgraphilerc.js", "to": "packages/functions/src/.postgraphilerc.js"},
-            ]
+          handler: "packages/functions/src/postgraphile_library.handler"
         }
-    },
+      },
     },
   });
   stack.addOutputs({
